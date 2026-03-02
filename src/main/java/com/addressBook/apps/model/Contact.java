4a3d7ac80c4c8de3401a871d37fbfdd664b772e9
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-03-02 22:19:02 +0530

    [Nageshwar] : added intial setter in Contact class

--- src/main/java/com/addressBook/apps/model/Contact.java
+++ src/main/java/com/addressBook/apps/model/Contact.java
@@ -50,6 +50,30 @@ public class Contact {
 		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
 				+ ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
 	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
 }

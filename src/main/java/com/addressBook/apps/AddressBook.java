60ee32212ec0951467958658a4fe1b7e9edcf6f0
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-03-06 10:24:54 +0530

    [Nageshwar] : added UC7 override equals methods

--- src/main/java/com/addressBook/apps/AddressBook.java
+++ src/main/java/com/addressBook/apps/AddressBook.java
@@ -8,7 +8,15 @@ public class AddressBook {
 	private List<Contact> contacts = new ArrayList<>(); //Helps to add multiple person in the Address book App
 	
 	public void addContact(Contact c) {
+		boolean exists = contacts.stream().anyMatch(contact	-> contact.getFirstName().equalsIgnoreCase(c.getFirstName())
+				&& contact.getLastName().equalsIgnoreCase(c.getLastName()));
+		
+		if(exists) {
+			System.out.println("Duplicate Contact! Person already exists.");
+			return;
+		}
 		contacts.add(c);
+		System.out.println("Contact added successfully");
 	}
 	public void displayContact() {
 		for(Contact c: contacts) {

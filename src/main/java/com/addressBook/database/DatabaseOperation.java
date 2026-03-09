08c6150277c878b02f6d0634f6186c68d800065f
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-03-09 12:55:43 +0530

    [Nageshwar] : UC18 database operation

--- src/main/java/com/addressBook/database/DatabaseOperation.java
+++ src/main/java/com/addressBook/database/DatabaseOperation.java
@@ -1,6 +1,8 @@
 package com.addressBook.database;
 import java.sql.*;
 import java.util.*;
+import java.time.LocalDate;
+import java.sql.Date;
 
 import com.addressBook.apps.model.Contact;
 
@@ -12,8 +14,8 @@ public class DatabaseOperation {
 		try {
 			con = ConnectDatabase.getConnection();
 			
-			String query = "insert into contacts(fname, lname, address, city, state, zip, phoneNo, email, addressBook)"
-					+ " values(?,?,?,?,?,?,?,?,?)";
+			String query = "insert into contacts(fname, lname, address, city, state, zip, phoneNo, email, addressBook, dateAdded)"
+					+ " values(?,?,?,?,?,?,?,?,?,?)";
 			
 			PreparedStatement pstm = con.prepareStatement(query);
 			pstm.setString(1, contact.getFirstName());
@@ -25,6 +27,7 @@ public class DatabaseOperation {
 			pstm.setString(7, contact.getPhoneNumber());
 			pstm.setString(8, contact.getEmail());
 			pstm.setString(9, addressBookName);
+			pstm.setDate(10, Date.valueOf(contact.getDateAdded()));
 			
 			int s = pstm.executeUpdate();
 			System.out.println(s+" rows updates sucessfully!");
@@ -57,7 +60,7 @@ public class DatabaseOperation {
 			c.setZip(rs.getString("zip"));
 			c.setPhoneNumber(rs.getString("phoneNo"));
 			c.setEmail(rs.getString("email"));
-			
+			c.setDate(rs.getDate("dateAdded"));
 			ans.add(c);
 		}
 		return ans;
